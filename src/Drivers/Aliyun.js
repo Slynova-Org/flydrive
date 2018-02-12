d0892342b472912e2d4ebc388c75b26aa5902dc6
borie88 <gpb8977@163.com>
2018-02-12 12:59:50 +0800

    Adds support for Aliyun OSS Storage Driver
    Requires ali-oss npm package npm install oli-oss --save

---
+++ src/Drivers/Aliyun.js
@@ -0,0 +1,211 @@
+const Resetable = require('resetable')
+const ali = require('ali-oss')
+
+/**
+ * Aliyun driver for using with flydriver
+ *
+ * @constructor
+ * @class Aliyun
+ */
+class Aliyun {
+  constructor (config) {
+    this.oss = ali.Wrapper(Object.assign({}, {
+      accessKeyId: config.key,
+      accessKeySecret: config.secret,
+      region: config.region,
+      secure: config.secure
+    }, config))
+
+    this._bucket = new Resetable(config.bucket)
+
+  }
+
+  /**
+   * Use a different bucket at runtime
+   *
+   * @method bucket
+   *
+   * @param  {String} bucket
+   *
+   * @chainable
+   */
+  bucket (bucket) {
+    this._bucket.set(bucket)
+    return this
+  }
+
+  /**
+   * Finds if a file exists or not
+   *
+   * @method exists
+   * @async
+   *
+   * @param  {String} location
+   * @param  {Object} [params]
+   *
+   * @return {Promise<Boolean>}
+   */
+  exists (location, params) {
+    const successStatuses = [200, 304]
+    return new Promise((resolve, reject) => {
+      this.oss.head(location, params = {})
+        .then((response) => {
+          if (successStatuses.indexOf(response.status) >= 0) {
+            resolve(true)
+          } else {
+            reject(response)
+          }
+        })
+        .catch(error => reject(error))
+    })
+  }
+
+  /**
+   * Create a new file from string or buffer
+   *
+   * @method put
+   * @async
+   *
+   * @param  {String} location
+   * @param  {String} content
+   * @param  {Object} [params]
+   *
+   * @return {Promise<String>}
+   */
+  put (location, content, params) {
+    return new Promise((resolve, reject) => {
+      this.oss.put(location, content)
+       .then(response => resolve(response.name))
+       .catch(error => reject(error))
+    })
+  }
+
+  /**
+   * Remove a file
+   *
+   * @method delete
+   * @async
+   *
+   * @param  {String} location
+   * @param  {Object} [params = {}]
+   *
+   * @return {Promise<Boolean>}
+   */
+  delete (location, params = {}) {
+    return new Promise((resolve, reject) => {
+      this.oss.delete(location, params)
+      .then(() => resolve(true))
+      .catch(error => reject(error))
+    })
+  }
+
+  /**
+   * Returns contents for a given file
+   *
+   * @method get
+   *
+   * @param  {String} location
+   * @param  {String} [file]
+   * @param  {Object} [params = {}]
+   *
+   * @return {Promise<String>}
+   */
+  get (location, file = '', params = {}) {
+    return new Promise((resolve, reject) => {
+      this.oss.get(location)
+      .then(response => {
+        const content = response.content
+        return Buffer.isBuffer(content) ? resolve(content) : reject(content)
+      })
+      .catch(error => reject(error))
+    })
+  }
+
+  /**
+   * Returns the stream for the given file
+   *
+   * @method getStream
+   *
+   * @param  {String}  location
+   * @param  {Object}  [params = {}]
+   *
+   * @return {Stream}
+   */
+  getStream (location, params = {}) {
+
+    return new Promise((resolve, reject) => {
+      this.oss.getStream(location, params)
+      .then(response => {
+        resolve(response.stream)
+      })
+      .catch(error => reject(error))
+    })
+  }
+
+  /**
+   * Returns url for a given key. Note this method doesn't
+   * validates the existence of file or it's visibility
+   * status.
+   *
+   * @method getUrl
+   *
+   * @param  {String} location
+   * @param  {String} bucket
+   *
+   * @return {String}
+   */
+  getUrl (location, bucket) {
+    bucket = bucket || this._bucket.pull()
+    const { secure, region } = this.oss.options
+    const protocol = secure ? 'https://' : 'http://'
+    return `${protocol}${bucket}.${region}.aliyuncs.com/${location}`
+  }
+
+  /**
+   * Copy file from one location to another within
+   * or accross oss buckets.
+   *
+   * @method copy
+   *
+   * @param  {String} src
+   * @param  {String} dest
+   * @param  {String} [destBucket = this.bucket]
+   * @param  {Object} [params = {}]
+   *
+   * @return {Promise<String>}
+   */
+  copy (src, dest, destBucket, params = {}) {
+    return new Promise((resolve, reject) => {
+      const bucket = this._bucket.pull()
+      const combinedDest = `/${bucket}/${src}`
+      // Aliyun OSS switches the src and destination for some reason
+      this.oss.copy(dest, combinedDest)
+        .then(response => {
+          resolve(this.getUrl(dest, destBucket))
+        })
+        .catch(error => reject(error))
+    })
+  }
+
+  /**
+   * Moves file from one location to another. This
+   * method will call `copy` and `delete` under
+   * the hood.
+   *
+   * @method move
+   *
+   * @param  {String} src
+   * @param  {String} dest
+   * @param  {String} [destBucket = this.bucket]
+   * @param  {Object} [params = {}]
+   *
+   * @return {Promise<String>}
+   */
+  async move (src, dest, destBucket, params = {}) {
+    const url = await this.copy(src, dest, destBucket, params)
+    await this.delete(src)
+    return url
+  }
+}
+
+module.exports = Aliyun
