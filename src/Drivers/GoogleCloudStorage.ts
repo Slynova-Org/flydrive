68b3606d29fd020062bc9a4c603ebe23589cd59b
Michaël Zasso <targos@protonmail.com>
2019-03-17 11:55:46 +0100

    fix(driver): return a new instance of GCS from bucket method (#85)

--- src/Drivers/GoogleCloudStorage.ts
+++ src/Drivers/GoogleCloudStorage.ts
@@ -11,11 +11,13 @@ import Storage from '../Storage'
 import { isReadableStream, pipeline } from '../utils'
 
 export class GoogleCloudStorage extends Storage {
+  protected $config: GoogleCloudStorageConfig
   protected $driver: GCSDriver
   protected $bucket: Bucket
 
   public constructor(config: GoogleCloudStorageConfig) {
     super()
+    this.$config = config
     const GCSStorage = require('@google-cloud/storage').Storage;
     this.$driver = new GCSStorage(config)
     this.$bucket = this.$driver.bucket(config.bucket)
@@ -27,9 +29,12 @@ export class GoogleCloudStorage extends Storage {
 
   /**
    * Use a different bucket at runtime.
+   * This method returns a new instance of GoogleCloudStorage.
    */
-  public bucket(name: string): void {
-    this.$bucket = this.$driver.bucket(name)
+  public bucket(name: string): GoogleCloudStorage {
+    const newStorage = new GoogleCloudStorage(this.$config)
+    newStorage.$bucket = newStorage.$driver.bucket(name)
+    return newStorage
   }
 
   /**
