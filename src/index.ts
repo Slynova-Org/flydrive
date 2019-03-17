a4dbdb4f67320b954f2762456f703ec1129478bf
Michaël Zasso <targos@protonmail.com>
2019-03-17 11:57:16 +0100

    feat(driver): export drivers and their types from entry point

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export { default as Storage } from './Storage'
 export { default as StorageManager } from './StorageManager'
+
+// export drivers and types
+export * from './Drivers/GoogleCloudStorage'
+export * from './Drivers/LocalFileSystem'
