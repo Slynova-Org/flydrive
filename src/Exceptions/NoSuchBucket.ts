662b5f9c88b451675264d15971859c93250bb952
Michaël Zasso <targos@protonmail.com>
2019-08-23 09:13:53 +0200

    fix: improve errors
    - Add a line with the original error message to avoid losing information.
    - Catch wrong key path error in GCS.
    - Catch 401 error in GCS

--- src/Exceptions/NoSuchBucket.ts
+++ src/Exceptions/NoSuchBucket.ts
@@ -10,7 +10,7 @@ import { RuntimeException } from 'node-exceptions';
 export class NoSuchBucket extends RuntimeException {
 	raw: Error;
 	constructor(err: Error, bucket: string) {
-		super(`The bucket ${bucket} doesn't exist`, 500, 'E_NO_SUCH_BUCKET');
+		super(`The bucket ${bucket} doesn't exist\n${err.message}`, 500, 'E_NO_SUCH_BUCKET');
 		this.raw = err;
 	}
 }
