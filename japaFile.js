75d4e09a305457e038d272c3b370a7892f39df81
Michaël Zasso <targos@protonmail.com>
2019-09-13 16:57:32 +0200

    now run tests

--- japaFile.js
+++ japaFile.js
@@ -5,6 +5,9 @@ const configure = require('japa').configure;
 configure({
 	files: ['test/**/*.spec.ts'],
 	filter: (file) => {
+		if (process.env.GITHUB_ACTION) {
+			return true;
+		}
 		if ((process.env.DOCKER && file.includes('gcs')) || (!process.env.DOCKER && file.includes('s3'))) {
 			return false;
 		}
--- test/unit/gcs-driver.spec.ts
+++ test/unit/gcs-driver.spec.ts
@@ -20,6 +20,7 @@ function streamToString(stream: Readable): Promise<string> {
 const testBucket = 'flydrive-test';
 const storage = new GoogleCloudStorage({
 	...(process.env.GCS_KEYFILENAME && { keyFilename: process.env.GCS_KEYFILENAME }),
+	...(process.env.GCS_KEY && { key: process.env.GCS_KEY }),
 	bucket: testBucket,
 });
 
